7bf75d9629323c3dc1f672c45d83be9ba0cd11c5
shvnsaha <shovan.css2018@gmail.com>
2025-10-26 19:38:11 +0600

    infinite scrolling part -2

--- src/components/PaginatedQueries.jsx
+++ src/components/PaginatedQueries.jsx
@@ -1,63 +1,62 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
-
-const fetchFruits = async ({ pageParam }) => {
-    return axios.get(`http://localhost:4000/fruits/?_limit=20&_page=${pageParam}`)
-}
-
+const fetchFruits = async ({ pageParam = 1 }) => {
+  const res = await axios.get(`http://localhost:4000/fruits?_limit=20&_page=${pageParam}`);
+  return res.data;
+};
 
 const PaginatedQueries = () => {
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ['fruits'],
+    queryFn: fetchFruits,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      // If fewer than 20 items were returned, we've reached the end
+      if (lastPage.length < 20) return undefined;
+      return allPages.length + 1;
+    },
+  });
 
+  const { ref, inView } = useInView();
 
-
-    const { data, isLoading, isError, error,fetchNextPage,hasNextPage } = useInfiniteQuery({
-        queryKey: ['fruits'],
-        queryFn: fetchFruits,
-        initialPageParam: 1,
-        getNextPageParam: (_lastPage, allPages) => {
-            if (allPages.length < 6) return allPages.length + 1;
-            else {
-                return undefined;
-            }
-        }
-
-        // staleTime: 30000,
-        // refetchInterval: 1000,
-        // refetchIntervalInBackground: true,
-        // enabled: true
-    })
-
-    // console.log(isLoading, isFetching);
-
-    if (isLoading) {
-        return <div>Page is Loading</div>
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
     }
-    if (isError) {
-        return <div>{error.message} </div>
-    }
+  }, [inView, hasNextPage, fetchNextPage, isFetchingNextPage]);
 
+  if (isLoading) return <div>Page is loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
-    console.log(data);
-
-    return (
-        <div className='container'>
-            {
-                data?.pages?.map(page =>{
-                    return page?.data.map(fruit => {
-                        return (
-                            <div key={fruit.id} className='fruit-item'>
-                                {fruit.name}
-                            </div>
-                        )
-                    })
-                })
-            }
-
-            <button onClick={()=>fetchNextPage()} disabled={!hasNextPage}>Load More</button>
-
+  return (
+    <div className="container">
+      {data?.pages?.map((page, i) => (
+        <div key={i}>
+          {page.map((fruit) => (
+            <div key={fruit.id} className="fruit-item">
+              {fruit.name}
+            </div>
+          ))}
         </div>
-    );
+      ))}
+
+      <div ref={ref} style={{ height: '30px' }} />
+
+      {isFetchingNextPage && <p>Loading more...</p>}
+      {!hasNextPage && <p>No more fruits to load 🍎</p>}
+    </div>
+  );
 };
 
 export default PaginatedQueries;
